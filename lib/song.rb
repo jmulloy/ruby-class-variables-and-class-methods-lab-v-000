c8a4c800b2cda40465efe0d7d79b2a00b3f7943b
Josh Mulloy <joshmulloy1@gmail.com>
2018-07-02 18:53:21 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,13 +2,16 @@ class Song
   def initialize(name)
     @name = name
   end
+class Genre
   def initialize(genre)
     @genre = genre
   end
+end
+  class Artist
   def initialize(artist)
     @artist = artist
   end
-
+end
 
 
 
