cd5a4ca26b3e188677b46651adf98370f3390301
Josh Mulloy <joshmulloy1@gmail.com>
2018-07-02 18:45:52 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,9 +2,13 @@ class Song
   def initialize(name)
     @name = name
   end
-    
+  def initialize(genre)
+    @genre = genre
   end
-  
+  def initialize(artist)
+    @artist = artist
+  end
+
   attr_accessor :name, :artist, :genres
   self.new(name, artist, genre)
 
